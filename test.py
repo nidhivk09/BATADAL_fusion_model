facea7f1539a0269749e056b5b6e003650f5d02b
Nidhi Kulkarni <nidhivk09@gmail.com>
2025-10-02 11:42:50 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("/Users/nidhikulkarni/ics-anomaly-detection/data/BATADAL/test_whitebox_attack.csv")
-
-count = (df["ATT_FLAG"] == 1).sum()
-
-print("anomaly count is:", count)
